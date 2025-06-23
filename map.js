6029905434aac98815dbd98c89438f6d892cfb76
Liam Owen <123574555+orangetreefrog4@users.noreply.github.com>
2025-06-23 21:07:49 +0100

    Map testing. The next few commits will probably just be "map testing". This is because I'm running this with github pages, and as such must push a commit every time I want to test something.

--- map.js
+++ map.js
@@ -4,4 +4,9 @@ function setupMap() {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
+    if ("geolocation" in navigator) {
+        alert("IT'S ALLIIIIVVVVEEEE")
+    } else {
+        alert("ERROR: GEOLOCATION ERROR\nThere was an error with geolocation data. Please make sure to give geolocation consent, and try resetting the page (not reloading) if the issue is recurring. If you believe this is a bug, please use the GitHub to report it. The link is at the bottom of the page.")
+    }
 }
