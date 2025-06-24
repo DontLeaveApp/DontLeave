9681f8167a92f54e87e17eb67351595019de9a5d
Liam Owen <123574555+orangetreefrog4@users.noreply.github.com>
2025-06-24 17:26:31 +0100

    testing map

--- map.js
+++ map.js
@@ -1,11 +1,14 @@
 function setupMap() {
-    var map = L.map('map').setView([51.505, -0.09], 13);
-    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
     if ("geolocation" in navigator) {
-        alert("IT'S ALLIIIIVVVVEEEE")
+        navigator.geolocation.getCurrentPosition((position) => {
+            doSomething(position.coords.latitude, position.coords.longitude)
+
+            var map = L.map('map').setView([position.coords.latitude, position.coords.longitude], 13);
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19,
+                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            }).addTo(map);;
+        });
     } else {
         alert("ERROR: GEOLOCATION ERROR\nThere was an error with geolocation data. Please make sure to give geolocation consent, and try resetting the page (not reloading) if the issue is recurring. If you believe this is a bug, please use the GitHub to report it. The link is at the bottom of the page.")
     }
